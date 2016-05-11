7d14b937f965478fe4b9a7f4583d0dc36f162f74
Lui <ruitang307@gmail.com>
2016-05-11 12:54:01 +0800

    fuck tencent

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -1,11 +1,13 @@
 export function convertImageUrl(url) {
-	// return url.replace(/http:\/\//, 'https://images.weserv.nl/?url=')
-	return 'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + url
+	return url.replace(/http:\/\//, 'https://images.weserv.nl/?url=')
+	// return 'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + url
+	// return url
 }
 
 export function convertDetailImageUrl(url) {
-	// return url.replace(/http\w{0,1}:\/\/pic/g, "https://images.weserv.nl/?url=pic")
-	return url.replace(/http\w{0,1}:\/\/pic/g, "http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=https://pic")
+	return url.replace(/http\w{0,1}:\/\/pic/g, "https://images.weserv.nl/?url=pic")
+	// return url.replace(/http\w{0,1}:\/\/pic/g, "http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=https://pic")
+	// return url
 }
 
 export function reachBottom() {
