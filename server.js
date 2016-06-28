16e437da5ab6ee32c40795f988b04eca189b963e
Lui <ruitang307@gmail.com>
2016-06-28 14:26:49 +0800

    added express middleware compression

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express')
 var path = require('path')
 var axios = require('axios')
 var http = require('http')
+var compression = require('compression')
 
 var app = express()
+
+app.use(compression())
 
 app.use(express.static(path.join(__dirname, 'public')))
 
